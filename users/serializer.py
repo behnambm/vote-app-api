4b51a49bfcc4cc762323922a0dc407088430f4c8
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-11-12 18:34:57 +0330

    add serializer for checking verification code check if the code is all digit and only 6 digits long

--- users/serializer.py
+++ users/serializer.py
@@ -1,5 +1,20 @@
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 
 class EmailSerializer(serializers.Serializer):
     email = serializers.EmailField()
+
+
+class CheckVerificationCodeSerializer(serializers.Serializer):
+    code = serializers.CharField()
+    email = serializers.EmailField()
+
+    def validate_code(self, value):
+        if not value.isdigit():
+            raise ValidationError(message="Only digits are allowed")
+
+        if not len(value) == 6:
+            raise ValidationError(message="Code is only 6 digits long")
+
+        return value
