0d5606222f791562daec95a581e1e58db80d9540
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-11-12 18:45:08 +0330

    add `api/user/email/checko/` this endpoint will be used to check email and verification code if code is correct `is_active` will be set to `True`

--- users/api.py
+++ users/api.py
@@ -2,10 +2,12 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializer import EmailSerializer
+from .models import Emails
+from .serializer import CheckVerificationCodeSerializer, EmailSerializer
 from .tasks import send_verification_code
 from .utils import (
     generate_verification_code,
+    is_code_correct,
     is_code_in_redis,
     set_verification_code,
 )
@@ -30,4 +32,31 @@ class EmailView(APIView):
         set_verification_code(user_email, code)
         send_verification_code.delay(email=user_email, code=code)
 
-        return Response({"msg": f"your code {code} -> {user_email}"})
+        return Response(
+            {"detail": f"please check your inbox at {user_email!r}"}
+        )
+
+
+class CheckVerificationCodeView(APIView):
+    throttle_scope = "email"
+
+    def post(self, request):
+        serializer = CheckVerificationCodeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        if not is_code_correct(
+            email=serializer.data.get("email"),
+            user_provided_code=serializer.data.get("code"),
+        ):
+            return Response(
+                {"detail": "Code is not correct"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        email_obj, _ = Emails.objects.get_or_create(
+            email=serializer.data.get("email")
+        )
+        email_obj.is_active = True
+        email_obj.save()
+
+        return Response({"detail": "successful"})
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .api import EmailView
+from .api import CheckVerificationCodeView, EmailView
 
 urlpatterns = [
     path("user/email/", EmailView.as_view()),
+    path("user/email/check/", CheckVerificationCodeView.as_view()),
 ]
