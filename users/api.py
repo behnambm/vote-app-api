aea328a257f3064354e0093248cb11fc8d7ed406
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-12-05 12:38:31 +0330

    add documentation to EmailView(send email)

--- users/api.py
+++ users/api.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -5,17 +8,47 @@ from rest_framework.views import APIView
 from .models import Emails
 from .serializer import CheckVerificationCodeSerializer, EmailSerializer
 from .tasks import send_verification_code
-from .utils import (
-    generate_verification_code,
-    is_code_correct,
-    is_code_in_redis,
-    set_verification_code,
-)
+from .utils import (generate_verification_code, is_code_correct,
+                    is_code_in_redis, set_verification_code)
 
 
 class EmailView(APIView):
     throttle_scope = "email"
 
+    @swagger_auto_schema(
+        operation_id="Send verification code",
+        operation_description=f"""
+        Sends an email containing 6-digit verification code 
+        """,
+        request_body=EmailSerializer,
+        responses={
+            200: openapi.Response(
+                description="ok",
+                examples={
+                    "application/json": {
+                        "detail": f"please check your inbox at 'user@example.com'"
+                    }
+                },
+            ),
+            202: openapi.Response(
+                description=f"wait {settings.VERIFICATION_CODE_EXPIRY} seconds",
+                examples={
+                    "application/json": {
+                        "detail": f"please wait {settings.VERIFICATION_CODE_EXPIRY} seconds"
+                    }
+                },
+            ),
+            400: openapi.Response(
+                description="invalid email",
+                examples={
+                    "application/json": {
+                        "email": ["Enter a valid email address."]
+                    }
+                },
+            ),
+        },
+        security=[],
+    )
     def post(self, request):
 
         serializer = EmailSerializer(data=request.data)
