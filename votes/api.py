262ece5c7e9d65ef738980f8b6f5d723d9f91bff
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-12-05 15:55:06 +0330

    add documentation for VotesView

--- votes/api.py
+++ votes/api.py
@@ -1,4 +1,6 @@
 from django.db.models import Q
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,6 +14,30 @@ from .serializer import VotesSerializer, VoteUpdateSerializer
 class VotesView(APIView):
     permission_classes = [ActiveEmailOnly]
 
+    @swagger_auto_schema(
+        operation_id="Get list of votes",
+        operation_description=f"""
+        Get list of votes 
+        """,
+        responses={
+            200: openapi.Response(
+                description="ok",
+                examples={
+                    "application/json": [
+                        {
+                            "id": 1,
+                            "title": "cats vs dogs",
+                            "slug": "cats-vs-dogs",
+                            "description": "test vote",
+                            "first_option": "cats",
+                            "second_option": "dogs",
+                        },
+                    ]
+                },
+            ),
+        },
+        security=[],
+    )
     def get(self, request):
         """Get list of votes
 
@@ -25,6 +51,40 @@ class VotesView(APIView):
         serializer = VotesSerializer(instance=votes, many=True)
         return Response(serializer.data)
 
+    @swagger_auto_schema(
+        operation_id="Insert or Update vote",
+        operation_description=f"""
+        Users can update/insert their vote.
+        """,
+        request_body=VoteUpdateSerializer,
+        responses={
+            200: openapi.Response(
+                description="ok",
+                examples={
+                    "application/json": {
+                        "detail": "successful",
+                        "user_choice": "cats",
+                    }
+                },
+            ),
+            404: openapi.Response(
+                description="vote does not exist",
+                examples={
+                    "application/json": {"detail": "vote does not exist"}
+                },
+            ),
+            400: openapi.Response(
+                description="wrong user choice",
+                examples={
+                    "application/json": {
+                        "detail": "'fox' is not a valid option",
+                        "valid_options": ["cats", "dogs"],
+                    }
+                },
+            ),
+        },
+        security=[],
+    )
     def put(self, request):
         """update if already exists or add new data to `votes.models.Voters`
 
